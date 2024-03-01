5147bbfcb3968f6e3f2981bb689394eb26f2b890
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2024-02-29 22:04:45 -0800

    new changes

--- data/create_table.py
+++ data/create_table.py
@@ -8,7 +8,7 @@ def format_date(timestamp):
 with open('header.md', 'r', encoding='utf-8') as header_file:
     header_content = header_file.read()
 
-with open('listings.json', 'r', encoding='utf-8') as file:
+with open('output.json', 'r', encoding='utf-8') as file:
     listings = json.load(file)
 
 markdown_content = "# Internship Listings for Summer 2024\n\n"
