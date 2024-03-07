99a5cb398c46f803f4a5af4ac6410dae519f9d69
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2024-03-07 13:20:21 -0800

    new changes

--- data/create_table.py
+++ data/create_table.py
@@ -28,9 +28,9 @@ for listing in listings:
     date_posted = format_date(listing.get('date_posted', '0'))
     terms = ', '.join(listing.get('terms', []))
     url = listing.get('url', '')
-    link_button = (f"[![Apply Button](data/image_url/applybutton.png)]({url})"
-                f"![Intern Insider](data/image_url/interninsidersmall.png)"
-                f"![Ribbon](data/image_url/ribbontsmall.png)")
+    link_button = (f"[![Apply Button](data/applybutton.png)]({url})"
+                f"![Intern Insider](data/interninsidersmall.png)"
+                f"![Ribbon](data/ribbonsmall.png)")
 
     markdown_content += f"| {company_name} | {title} | {locations} | {date_posted} | {terms} | {link_button} |\n"
 
