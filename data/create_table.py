e3e516a4ffe76e07844c81907140ebb62db4152f
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2024-03-08 15:57:50 -0800

    new changes

--- data/create_table.py
+++ data/create_table.py
@@ -26,8 +26,8 @@ for listing in listings:
     terms = ', '.join(listing.get('terms', []))
     url = listing.get('url', '')
     link_button = (f'<a href="{url}" target="_blank"><img src="data/images/applybutton.png" alt="Apply Button" style="width:80px;"></a>'
-                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:45px;"></a>'
-                    f'<a href="https://www.ribbon.ai/install?utm_source=github" target="_blank"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:45px;"></a>')
+                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:40px;"></a>'
+                    f'<a href="https://www.ribbon.ai/install?utm_source=github" target="_blank"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:40px;"></a>')
 
     markdown_content += f"| {company_name} | {title} | {locations} | {link_button} | {date_posted} |\n"
 
