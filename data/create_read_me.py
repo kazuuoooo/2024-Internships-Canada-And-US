0bf585c8cd13c38804002f37012d2c02f0100131
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2024-03-08 18:24:41 -0800

    new chagnes

--- data/create_read_me.py
+++ data/create_read_me.py
@@ -44,8 +44,7 @@ for listing in listings:
     apply_url = listing.get('Apply', '')
     apply_url_encoded = urllib.parse.quote(apply_url, safe=':/')
     link_button = (f'<a href="{apply_url_encoded}" target="_blank"><img src="data/images/applybutton.png" alt="Apply Button" style="width:80px;"></a>'
-                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:40px;"></a>'
-                    f'<a href="https://www.ribbon.ai/install" target="_blank"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:40px;"></a>')
+                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:40px;"></a>')
 
     markdown_content += f"| {company_name} | {role} | {locations} | {link_button} | {date_posted} |\n"
 
