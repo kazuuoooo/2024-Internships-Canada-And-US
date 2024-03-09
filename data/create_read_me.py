1aa68880fbeda6224c5dbcbd73cb87832051bc5f
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2024-03-08 17:54:33 -0800

    new changes

--- data/create_read_me.py
+++ data/create_read_me.py
@@ -21,8 +21,8 @@ for listing in listings:
     date_posted = listing.get('Date Posted', 'N/A') 
     apply_url = listing.get('Apply', '')
     link_button = (f'<a href="{apply_url}" target="_blank"><img src="data/images/applybutton.png" alt="Apply Button" style="width:80px;"></a>'
-                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:40px;"></a>'
-                    f'<a href="https://www.ribbon.ai/install" target="_blank"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:40px;"></a>')
+                    f'<a href="https://www.interninsider.me/subscribe?utm_source=githubposting" target="_blank"><img src="data/images/interninsidersmall.png" alt="Intern Insider" style="width:35px;"></a>'
+                    f'<a href="https://www.ribbon.ai/install" target="_blank"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:35px;"></a>')
 
     markdown_content += f"| {company_name} | {role} | {locations} | {link_button} | {date_posted} |\n"
 
